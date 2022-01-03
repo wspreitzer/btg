9a99590b99d19fa4df22ffeb3790115eec5bcafc
William Spreitzer <wspreitzer2@comcast.net>
2022-01-03 00:28:51 -0600

    Initial Project Creation Commit

---
+++ repo/btg/backend/src/test/java/com/btg/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btg.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
