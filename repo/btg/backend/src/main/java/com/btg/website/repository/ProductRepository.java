7e596dca806270829560f99490c35a9c799ec758
William Spreitzer <wspreitzer2@comcast.net>
2022-01-08 21:40:14 -0600

    Adding Products Specification Feature

---
+++ repo/btg/backend/src/main/java/com/btg/website/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.btg.website.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import com.btg.website.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+}
