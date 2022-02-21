a4f60c89bf2a49bde5f0b07ddd6eeae1ab2792b4
William Spreitzer <wspreitzer2@comcast.net>
2022-02-21 11:52:43 -0600

    Fixed error in BtgUtils.buildSearchCriteria(BtgSpecificationBuilder<T> builder, String search).  builder.with was using wrong signature

--- repo/btg/backend/src/main/java/com/btg/website/util/BtgUtils.java
+++ repo/btg/backend/src/main/java/com/btg/website/util/BtgUtils.java
@@ -47,9 +47,9 @@ public class BtgUtils {
 		Matcher matcher = pattern.matcher(search + ",");
 		while(matcher.find()) {
 			if(matcher.groupCount() == 3) {
-				builder.with(matcher.group(1), matcher.group(2), "", matcher.group(3), "" );
+				builder.with(matcher.group(1), matcher.group(2), matcher.group(3), "", "" );
 			} else if (matcher.groupCount() == 5) {
-				builder.with(matcher.group(1), matcher.group(2), matcher.group(4), matcher.group(3), matcher.group(5));
+				builder.with(matcher.group(1), matcher.group(2), matcher.group(3), matcher.group(4), matcher.group(5));
 			} else {
 				throw new InvalidRequestException();
 			}
