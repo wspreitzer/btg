7f0b2e789c069c4adb2f8f5017160387c58f47da
William Spreitzer <wspreitzer2@comcast.net>
2022-02-22 18:41:08 -0600

    changed logic for checking count of params

--- repo/btg/backend/src/main/java/com/btg/website/repository/builder/BtgSpecificationBuilder.java
+++ repo/btg/backend/src/main/java/com/btg/website/repository/builder/BtgSpecificationBuilder.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
+
+import com.btg.website.exception.InvalidRequestException;
 import com.btg.website.repository.specification.BtgSpecification;
 import com.btg.website.util.SearchCriteria;
 import com.btg.website.util.SearchOperation;
@@ -50,23 +52,22 @@ private List<SearchCriteria> params;
 	
 	public  Specification<T> build(Function<SearchCriteria, BtgSpecification<T>> converter) {
 		Specification<T> result;
-		if(params.size() == 0) {
-			result = null;
-		}
-		
-		final List<Specification<T>> specs = params.stream()
-				.map(converter)
-				.collect(Collectors.toCollection(ArrayList::new));
-		result = specs.get(0);
-		for(int i = 1; i < specs.size(); i++) {
-			result = params.get(i)
-					.isOrPredicate()
+		if(params.size() > 0) {
+			final List<Specification<T>> specs = params.stream()
+					.map(converter)
+					.collect(Collectors.toCollection(ArrayList::new));
+			result = specs.get(0);
+			for(int i = 1; i < specs.size(); i++) {
+				result = params.get(i)
+						.isOrPredicate()
 						? Specification.where(result)
 								.or(specs.get(i))
-						: Specification.where(result)
+								: Specification.where(result)
 								.and(specs.get(i));
+			}
+		} else {
+			throw new InvalidRequestException();
 		}
-		
 		return result;
 	}
 }
