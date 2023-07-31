cc97e706e1de0bc44ce675fdde29f005deb01cf5
Bill <wspreitzer2@comcast.net>
2023-07-31 15:06:22 -0500

    Fixing error in CartRestController where Product object was defined instead of ProductRepository

--- repo/btg/backend/src/main/java/com/btg/website/controller/CartRestController.java
+++ repo/btg/backend/src/main/java/com/btg/website/controller/CartRestController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.btg.website.model.Cart;
-import com.btg.website.model.Product;
 import com.btg.website.repository.CartItemRepository;
 import com.btg.website.repository.CartRepository;
 import com.btg.website.repository.CustomerRepository;
+import com.btg.website.repository.ProductRepository;
 
 @RestController
 public class CartRestController {
@@ -18,7 +18,7 @@ public class CartRestController {
 	@Autowired private CartRepository cartRepo;
 	@Autowired private CustomerRepository customerRepo;
 	@Autowired private CartItemRepository cartItemRepo;
-	@Autowired private Product productRepo;
+	@Autowired private ProductRepository productRepo;
 	
 	@GetMapping("/rest/cart/{id}")
 	public EntityModel<Cart> getCartById(@PathVariable Long id) {
